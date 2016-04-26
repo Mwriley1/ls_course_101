64c4c47c1e7b49779b6e15a16f59c2e2a8f32f42
Michael Riley <mwriley1@gmail.com>
2016-04-26 16:04:01 -0400

    Updated tic_tac_toe.rb

--- lesson_4/tic_tac_toe.rb
+++ lesson_4/tic_tac_toe.rb
@@ -20,38 +20,113 @@ def display_move_instructions(player_marker)
 	instructions.each {|line| puts(line.center(100))}
 end
 
-def select_square(square, square_values, marker)
-	square_values[square].replace(marker)
+def player_select_square(square, square_values, marker)
+  loop do
+    if square != '0' && square.to_i == 0 || square_values[square.to_i] != ' '
+      puts 'That is not a valid choice.'
+      puts 'Please select a square.'
+      square = gets.chomp
+    else
+      square_values[square.to_i].replace(marker)
+      break
+    end
+  end
 end
 
-def computer_move(square_values, computer_marker, player_marker)
-	if square_values[4] == ' '
-		select_square(4, square_values, computer_marker)
-	else
-		square_values.each do |square|
-			if square == ' '
-			select_square(4, square_values, computer_marker)
-		end
-	end
+def search_board(square_values, winning_combos, player_marker, computer_marker, action)
+  moves = nil
+  open_spaces = nil
+  marker = nil
+  
+  case action.downcase
+  when 'win'
+    moves = 2
+    open_spaces = 1
+    marker = computer_marker
+  when 'block'
+    moves = 2
+    open_spaces = 1
+    marker = player_marker
+  when 'open'
+    moves = 1
+    open_spaces = 2
+    marker = computer_marker
+  end
+
+  test_array = []
+  winning_combos.each do |combo|
+    3.times {|int| test_array[int] = square_values[combo[int]]}
+    if test_array.count(marker) == moves && test_array.count(' ') == open_spaces
+      return combo[test_array.index(' ')]
+    end
+  end
+  nil
 end
 
-def win?(board_values, marker)
-	winning_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4 ,6]]
-
+def pick_open_spot(square_values, winning_combos, player_marker, computer_marker)
+  corners_array = [square_values[0], square_values[2], square_values[6], square_values[8]]
+  if search_board(square_values, winning_combos, player_marker, computer_marker, 'open')
+    move = search_board(square_values, winning_combos, player_marker, computer_marker, 'open')
+  elsif square_values[4] == ' '
+    move = 4
+  elsif corners_array.include?(' ')
+    case corners_array.index(' ')
+    when 0
+      move = 0
+    when 1 
+      move = 2
+    when 2
+      move = 6
+    else
+      move = 8
+    end
+  else
+    move = square_values.index(' ')
+  end
 end
 
+def computer_move(square_values, winning_combos, player_marker, computer_marker)
+  if search_board(square_values, winning_combos, player_marker, computer_marker, 'win')
+    move = search_board(square_values, winning_combos, player_marker, computer_marker, 'win')
+  elsif search_board(square_values, winning_combos, player_marker, computer_marker, 'block')
+    move = search_board(square_values, winning_combos, player_marker, computer_marker, 'block')
+  else
+    move = pick_open_spot(square_values, winning_combos, player_marker, computer_marker)
+  end
+  square_values[move].replace(computer_marker)
+end
 
+def win_game?(square_values, winning_combos, player_marker, computer_marker)
+  test_array = []
+  winning_combos.each do |combo|
+    3.times {|int| test_array[int] = square_values[combo[int]]}
+    if test_array.count(player_marker) == 3 
+      return player_marker
+    elsif test_array.count(computer_marker) == 3
+      return computer_marker
+    end
+  end
+  false
+end
 
-
-
+def display_winner(square_values, winning_combos, player_marker, computer_marker, player_score, computer_score)
+  if win_game?(square_values, winning_combos, player_marker, computer_marker) == player_marker
+    display_board(square_values, player_score, computer_score, player_marker, computer_marker)
+    puts 'You win!'
+  else
+    display_board(square_values, player_score, computer_score, player_marker, computer_marker)
+    puts 'The computer wins!'
+  end
+end
 
 #-----------------------------
 
-square_values = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-player_marker = ''
-computer_marker = ''
+winning_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4 ,6]]
 player_score = 0
 computer_score = 0
+
+
+#=begin
 
 puts "Welcome to Tic Tac Toe!\n\n"
 
@@ -62,23 +137,44 @@ loop do
 	if start_game == 'Q'
 		break
 	elsif start_game == 'N'
-		select_player_marker(player_marker)
+		square_values = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    player_marker = ''
+    computer_marker = ''
+
+    select_player_marker(player_marker)
 		select_computer_marker(player_marker, computer_marker)
 		
 		loop do
-			display_board(square_values, player_score, computer_score, player_marker, computer_marker)
-			display_move_instructions(player_marker)
-			player_selection = gets.chomp.to_i
+      if win_game?(square_values, winning_combos, player_marker, computer_marker)
+        if win_game?(square_values, winning_combos, player_marker, computer_marker) == player_marker
+          display_winner(square_values, winning_combos, player_marker, computer_marker, player_score, computer_score)
+          player_score += 1
+          break
+        else
+          display_winner(square_values, winning_combos, player_marker, computer_marker, player_score, computer_score)
+          computer_score += 1
+          break
+        end
+      end
 
-			select_square(player_selection, square_values, player_marker)
-			p square_values
-			#puts win?(square_values, player_marker)
-			break if win?(square_values, player_marker)
+      display_board(square_values, player_score, computer_score, player_marker, computer_marker)
+			display_move_instructions(player_marker)
+			
+      player_selection = gets.chomp
+			player_select_square(player_selection, square_values, player_marker)
+      display_board(square_values, player_score, computer_score, player_marker, computer_marker)
+
+      if square_values.include?(' ') == false && win_game?(square_values, winning_combos, player_marker, computer_marker) == false
+        puts "It's a draw!"
+        break
+      end
+
+			computer_move(square_values, winning_combos, player_marker, computer_marker)
 		end
 	else
 		puts 'That is not a valid choice.'
 	end
 end
 
-
+#=end
 
